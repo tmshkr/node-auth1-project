65c61e4a3d461db18bbcc1a74e6e154ccb88522f
Tim Shaker <tim@tmshkr.com>
2020-04-20 12:44:47 -0700

    POST /api/register

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const router = require("express").Router();
 const Users = require("./users-model.js");
 
@@ -6,7 +7,25 @@ router.get("/users", (req, res) => {
     .then((users) => {
       res.json(users);
     })
-    .catch((err) => res.send(err));
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("There was a problem getting the users");
+    });
+});
+
+router.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  const rounds = process.env.HASH_ROUNDS || 8;
+  const hash = bcrypt.hashSync(password, rounds);
+
+  Users.add({ username, password: hash })
+    .then(() => {
+      res.status(201).send("New user created");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("There was a problem creating the user");
+    });
 });
 
 router.get("/", (req, res) => {
