730eeab909e4d75fa9ec19bd8142da043e99cc8a
Tim Shaker <tim@tmshkr.com>
2020-04-20 13:33:25 -0700

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import "./App.scss";
 
 function App() {
-  return <main className="App">hello world</main>;
+  return (
+    <Router>
+      <main className="App">
+        <header className="top">
+          <nav>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+          </nav>
+        </header>
+
+        <Switch>
+          <Redirect to="/login" />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
