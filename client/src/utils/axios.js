76c6a25d6a94d1f8e8e871aefa28d77c1dada2f3
Tim Shaker <tim@tmshkr.com>
2020-04-20 15:41:11 -0700

    update axios

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const instance = axios.create({
+export default axios.create({
   baseURL: "http://localhost:5000",
   withCredentials: true,
 });
-export default instance;
